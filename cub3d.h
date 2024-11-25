6b849ff720f3b91d362e795a3f2f67eaec126d46
tamasandor <111676834+tamasandor@users.noreply.github.com>
2024-11-25 16:08:18 +0100

    Walls with textures on the screen (#5)
    * Walls with textures on the screen, some textures and config modification, basic wall collision (needs to be worked on), corrected parsing issues (swapped x and y), connected parsing to set up functions
    
    * Add free_parsing when img is returning NULL and better error message, use the same variable as above for the player orientation

--- cub3d.h
+++ cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hzakharc < hzakharc@student.42wolfsburg    +#+  +:+       +#+        */
+/*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 08:30:34 by atamas            #+#    #+#             */
-/*   Updated: 2024/11/12 15:45:49 by hzakharc         ###   ########.fr       */
+/*   Updated: 2024/11/25 09:41:26 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@
 
 # define SCREEN_X 1024
 # define SCREEN_Y 720
+# define TEXTURE_X 64
+# define TEXTURE_Y 64
 # define KEY_W 119
 # define KEY_A 97
 # define KEY_S 115
@@ -30,6 +32,7 @@
 # define KEY_ESC 65307
 # define KEY_LEFT 65361
 # define KEY_RIGHT 65363
+# define BLOCK_SIZE 32
 # define PI 3.141592
 # define RED 0x00FF0000
 
@@ -59,52 +62,86 @@ typedef struct s_parse
 	int		f_color;
 }	t_parse;
 
-typedef struct s_struct
+typedef struct s_textures
 {
-	void	*mlx;
-	void	*mlx_win;
-
 	void	*img;
+	int		width;
+	int		height;
 	char	*addr;
 	int		b_p_p;
 	int		line_length;
 	int		endian;
-	double	player_x;
-	double	player_y;
-	double	dir_x;
-	double	dir_y;
-	double	plane_x;
-	double	plane_y;
-	char	player_orient;
-	//
-	double	camera_x;
-	double	deltadist_x;
-	double	deltadist_y;
-	double	perpWall;
-	double	move_speed;
-	double	rotate_speed;
-	int		hit;
-	int		side;
+}	t_textures;
+
+typedef struct s_struct
+{
+	void		*mlx;
+	void		*mlx_win;
+	t_parse		*parse;
+	t_textures	texture[4];
+	void		*img;
+	char		*addr;
+	int			b_p_p;
+	int			line_length;
+	int			endian;
+	double		player_x;
+	double		player_y;
+	double		dir_x;
+	double		dir_y;
+	double		plane_x;
+	double		plane_y;
+	char		player_orient;
+	int			prev_x;
+	double		move_speed;
+	double		rotate_speed;
 }	t_struct;
 
-void	my_mlx_pixel_put(t_struct *data, int x, int y, int color);
-void	clear_screen(t_struct *mlx);
-void	draw_square(int size, int x, int y, int color, t_struct *game);
-void	draw_triangle(int size, int x, int y, int color, t_struct *mlx);
-void	rotate(int keycode, t_struct *mlx);
-void	movement(int keycode, t_struct *mlx);
-int		mlx_setup(t_struct *mlx);
-void	set_up_player(t_struct *mlx);
-int		clean_exit(t_struct *mlx);
+typedef struct s_ray
+{
+	t_struct	*mlx;
+	t_textures	t;
+	double		camera_x;
+	double		ray_dir_x;
+	double		ray_dir_y;
+	int			map_x;
+	int			map_y;
+	double		deltadist_x;
+	double		deltadist_y;
+	int			step_x;
+	int			step_y;
+	double		side_dist_x;
+	double		side_dist_y;
+	double		wall_dist;
+	int			line_height;
+	int			draw_start;
+	int			draw_end;
+	int			side;
+	double		wall_x;
+	int			p_color;
+}	t_ray;
+
+void			my_mlx_pixel_put(t_struct *data, int x, int y, int color);
+unsigned int	my_mlx_pixel_get(t_textures *data, int x, int y);
+void			clear_screen(t_struct *mlx);
+void			draw_map(t_struct *mlx);
+void			rotate(int keycode, t_struct *mlx);
+void			movement(int keycode, t_struct *mlx);
+int				mlx_setup(t_struct *mlx);
+void			set_up_player(t_struct *mlx);
+int				clean_exit(t_struct *mlx);
+void			ray_cast(t_struct *mlx);
+void			line_height(t_ray *ray);
+void			choose_img_side(t_ray *ray, t_struct *mlx);
 
 //parsing functions
-void	parse_file(char **av, t_parse *parse);
-void	check_files(char **av, int ac);
-void	err_inc_parse(char *first_message);
-void	parse_textures(t_parse *parse);
-void	parse_colors(t_parse *parse);
-int		find_map(t_parse *parse);
-void	find_player(t_parse *parse);
-void	trim_map(t_parse *parse);
+void			parse_file(char **av, t_parse *parse);
+void			check_files(char **av, int ac, t_parse *parse);
+void			err_inc_parse(char *first_message);
+void			parse_textures(t_parse *parse);
+void			parse_colors(t_parse *parse);
+int				find_map(t_parse *parse);
+void			find_player(t_parse *parse);
+void			trim_map(t_parse *parse);
+void			free_parse(t_parse *parse);
 
 #endif
--- mlx.c
+++ mlx.c
@@ -6,7 +6,7 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 08:30:15 by atamas            #+#    #+#             */
-/*   Updated: 2024/11/05 13:21:52 by atamas           ###   ########.fr       */
+/*   Updated: 2024/11/25 09:44:51 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,46 @@
 #include "./minilibx/mlx_int.h"
 #include "cub3d.h"
 
-void	my_mlx_pixel_put(t_struct *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->b_p_p / 8));
-	*(unsigned int *)dst = color;
-}
-
 int	clean_exit(t_struct *mlx)
 {
 	mlx_destroy_image(mlx->mlx, mlx->img);
+	mlx_destroy_image(mlx->mlx, mlx->texture[NO].img);
+	mlx_destroy_image(mlx->mlx, mlx->texture[SO].img);
+	mlx_destroy_image(mlx->mlx, mlx->texture[EA].img);
+	mlx_destroy_image(mlx->mlx, mlx->texture[WE].img);
 	mlx_destroy_window(mlx->mlx, mlx->mlx_win);
 	mlx_destroy_display(mlx->mlx);
 	free(mlx->mlx);
+	free_parse(mlx->parse);
 	exit(0);
 }
 
 int	event_handler(int keycode, t_struct *mlx)
 {
 	if (keycode == KEY_ESC)
-	{
 		clean_exit(mlx);
-	}
-	if (keycode == KEY_A || keycode == KEY_D || keycode == KEY_S || keycode == KEY_W)
+	if (keycode == KEY_A || keycode == KEY_D
+		|| keycode == KEY_S || keycode == KEY_W)
 		movement(keycode, mlx);
 	if (keycode == KEY_LEFT || keycode == KEY_RIGHT)
 		rotate(keycode, mlx);
 	clear_screen(mlx);
-	draw_triangle(25, mlx->player_x, mlx->player_y, RED, mlx);
-	// draw_square(50, mlx->player_x, mlx->player_y, RED, mlx);
+	ray_cast(mlx);
 	mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, mlx->img, 0, 0);
 	return (0);
 }
 
 int	mouse_move(int x, int y, t_struct *mlx)
 {
-	printf("x: %d, y: %d\n", x, y);
-	// my_mlx_pixel_put(mlx, x, y, 0x00FF0000);
+	if (x < mlx->prev_x)
+		rotate(KEY_LEFT, mlx);
+	else
+		rotate(KEY_RIGHT, mlx);
+	mlx->prev_x = x;
+	clear_screen(mlx);
+	ray_cast(mlx);
 	mlx_put_image_to_window(mlx->mlx, mlx->mlx_win, mlx->img, 0, 0);
-	return (0);
+	return (y - y);
 }
 
 int	mlx_setup(t_struct *mlx)
@@ -68,6 +68,7 @@ int	mlx_setup(t_struct *mlx)
 	mlx_hook(mlx->mlx_win, 2, 1L << 0, event_handler, mlx);
 	// mlx_hook(mlx->mlx_win, 6, 1L << 6, mouse_move, mlx);
 	mlx->img = mlx_new_image(mlx->mlx, SCREEN_X, SCREEN_Y);
-	mlx->addr = mlx_get_data_addr(mlx->img, &mlx->b_p_p, &mlx->line_length, &mlx->endian);
+	mlx->addr = mlx_get_data_addr(mlx->img, &mlx->b_p_p,
+			&mlx->line_length, &mlx->endian);
 	return (0);
 }
